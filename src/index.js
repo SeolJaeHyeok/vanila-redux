0028f76d9c41aa09877acaa781ed68e56f01641d
seoljaehyeok <milkboy2564@naver.com>
2021-02-17 15:41:21 +0900

    Improvement

--- src/index.js
+++ src/index.js
@@ -6,15 +6,19 @@ const number = document.querySelector("span");
 
 number.innerText = 0;
 
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 // createStore의 reducer => 유일하게 data를 modify 할 수 있음
 // 그렇다면 어떻게 수정하느냐 => actions를 통해 data modify 가능
 const countModifier = (count = 0, action) => {
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -24,14 +28,15 @@ const onChange = () => {
   number.innerText = countStore.getState();
 };
 
+// store의 변화를 감지하면 function 실행
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 };
 
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
