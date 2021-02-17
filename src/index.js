4e1c43335474cd006d42eddc9459af59a7e839e1
seoljaehyeok <milkboy2564@naver.com>
2021-02-17 15:16:42 +0900

    action와 dispatch  이용해 reducer에 접근 후 data modifying

--- src/index.js
+++ src/index.js
@@ -6,10 +6,24 @@ const number = document.querySelector("span");
 
 // createStore의 reducer => 유일하게 data를 modify 할 수 있음
 // 그렇다면 어떻게 수정하느냐 => actions를 통해 data modify 가능
-const countModifier = (count = 0) => {
-  return count;
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
 
 console.log(countStore.getState());
