b48c0e703f17c4d01dbaa3c73fb661b36b789f11
seoljaehyeok <milkboy2564@naver.com>
2021-02-18 15:29:16 +0900

    Improvement

--- src/index.js
+++ src/index.js
@@ -19,10 +19,12 @@ const deleteToDoAction = (id) => {
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
+      const newObj = { text: action.text, id: Date.now() };
       // state를 변형(mutate)하는 것이 아니라 ex)state.push() 새로운 state를 return 해야만 한다. 새로운 state를 이전의 state와 새로운 toDo를 가짐
-      return [{ text: action.text, id: Date.now() }, ...state];
+      return [newObj, ...state];
     case DELETE_TODO:
-      return state.filter((toDo) => toDo.id !== action.id);
+      const cleaned = state.filter((toDo) => toDo.id !== action.id);
+      return cleaned;
     default:
       return state;
   }
