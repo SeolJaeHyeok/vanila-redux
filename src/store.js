b18d01315af716441a8521e865fe4da7fc477711
seoljaehyeok <milkboy2564@naver.com>
2021-02-20 22:14:01 +0900

    createSlice로 actions와 reducer 함께 구현

--- src/store.js
+++ src/store.js
@@ -1,24 +1,18 @@
-import { createStore } from "redux";
-import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const addToDo = createAction("ADD");
-const deleteToDo = createAction("DELETE");
-
-// createReducer를 사용하면 state를 mutate 하기 쉽게 만들어준다.
-const reducer = createReducer([], {
-  [addToDo]: (state, action) => {
-    state.push({ text: action.payload, id: Date.now() }); // state mutation
+//createSlice로 createAction과 createReducer 함께 구현
+const toDos = createSlice({
+  name: "toDosReducer",
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() }); // state mutation
+    },
+    remove: (state, action) =>
+      state.filter((toDo) => toDo.id !== action.payload),
   },
-  [deleteToDo]: (state, action) =>
-    state.filter((toDo) => toDo.id !== action.payload),
 });
 
-// configureStore를 쓰면 Redux Developer Tool 사용가능
-const store = configureStore({ reducer });
+export const { add, remove } = toDos.actions;
 
-export const actionCreators = {
-  addToDo,
-  deleteToDo,
-};
-
-export default store;
+export default configureStore({ reducer: toDos.reducer });
